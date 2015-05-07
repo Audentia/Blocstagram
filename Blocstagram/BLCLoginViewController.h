a6db89cfe99f9a70dd02f8e75cb30683716a734b
Douglas Hewitt <doughewitt4@gmail.com>
2015-05-07 15:31:38 -0700

    Add Instagram Login

---
+++ Blocstagram/BLCLoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  BLCLoginViewController.h
+//  Blocstagram
+//
+//  Created by Douglas Hewitt on 5/6/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCLoginViewController : UIViewController
+
+extern NSString *const BLCLoginViewControllerDidGetAccessTokenNotification;
+
+@end
