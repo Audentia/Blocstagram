8cd955fed6cc0f4ea8d3df9a823bc2a6264b2264
Douglas Hewitt <doughewitt4@gmail.com>
2015-05-20 23:24:25 -0700

    Added Photo Library ASSIGNMENT NOT DONE

---
+++ Blocstagram/BLCCropBox.h
@@ -0,0 +1,13 @@
+//
+//  BLCCropBox.h
+//  Blocstagram
+//
+//  Created by Douglas Hewitt on 5/20/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCCropBox : UIView
+
+@end
